76fdb33aa4ade455ea4a4a475b504d2128ba2742
asifsouravgit <asifahmedsourav@gmail.com>
2024-06-21 23:07:38 +0600

    Part 1 Committed

---
+++ IntroToCSharp/IntroToCSharp/Part1.cs
@@ -0,0 +1,13 @@
+﻿using System; //Namespace declaration
+
+namespace IntroToCSharp
+{
+    class Part1
+    {
+        static void Main() //Entry point of the program
+        {
+            Console.WriteLine("Welcome to C# Trainning 2024");
+            Console.ReadLine();
+        }
+    }
+}
