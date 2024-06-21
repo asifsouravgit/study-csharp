7220616db510ea3e7fe1233e098b8385bbe3c4dd
asifsouravgit <asifahmedsourav@gmail.com>
2024-06-22 01:16:04 +0600

    Part 2 Committed

--- IntroToCSharp/IntroToCSharp/Part1.cs
+++ IntroToCSharp/IntroToCSharp/Part1.cs
@@ -1,10 +1,10 @@
-﻿using System; //Namespace declaration
+﻿using System; /** Namespace declaration **/
 
 namespace IntroToCSharp
 {
     class Part1
     {
-        static void Main() //Entry point of the program
+        static void MainOfPart1() /** Entry point of the program **/
         {
             Console.WriteLine("Welcome to C# Trainning 2024");
             Console.ReadLine();
---
+++ IntroToCSharp/IntroToCSharp/Part2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToCSharp
+{
+    class Part2
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter your name below.");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + userName); //Using Concatenation
+            Console.WriteLine("Hello {0}", userName); //Using Placeholder
+            
+            Console.ReadLine(); //For Pause
+
+            Console.WriteLine("Please enter your first name below.");
+            string firstName = Console.ReadLine();
+            
+            Console.WriteLine("Please enter your last name below.");
+            string lastName = Console.ReadLine();
+            
+            Console.WriteLine("Hello {0} {1}", firstName, lastName);
+            Console.ReadLine(); //For Pause
+        }
+    }
+}
